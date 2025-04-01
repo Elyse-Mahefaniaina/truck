d6ecdac7b2b12955d915d6e59b563c85b2985971
Elyse Mahefaniaina <elysemahefaniaina@gmail.com>
2025-04-01 21:03:41 +0300

    front_added_all point

--- src/components/leaflet-routing-machine/LeafletRoutingMachine.js
+++ src/components/leaflet-routing-machine/LeafletRoutingMachine.js
@@ -13,48 +13,13 @@ const LeafletRoutingMachine = ({ type }) => {
   const [currentMarker, setCurrentMarker] = useState(null)
   const [pickupMarker, setPickupMarker] = useState(null);
   const [dropoffMarker, setDropoffMarker] = useState(null);
-  const [routeData, setRouteData] = useState({ distance: 0, duration: 0, coordinates: [] });
   const [routingControl, setRoutingControl] = useState(null);
-  const [remainingFuelDistance, setRemainingFuelDistancel] = useState(0);
-  const [isGasStationFind, setGasStationFind] = useState(false);
-
-  const getGasStations = async (lat, lng, radius = 1000) => {
-    const query = `
-      [out:json];
-      (
-        node["amenity"="fuel"](around:${radius},${lat},${lng});
-        way["amenity"="fuel"](around:${radius},${lat},${lng});
-        relation["amenity"="fuel"](around:${radius},${lat},${lng});
-      );
-      out body;
-    `;
-    
-    const url = `https://overpass-api.de/api/interpreter?data=${encodeURIComponent(query)}`;
   
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      return data.elements.map(element => ({
-        lat: element.lat,
-        lng: element.lon,
-        name: element.tags.name || "Unnamed Station"
-      }));
-    } catch (error) {
-      console.error("Error fetching gas stations:", error);
-      return [];
-    }
-  };
-  
-  
-
-  useEffect(() => {
-    api.post("/api/trip")
-    .then((response) => {
-      setRemainingFuelDistancel(1000 - response.data.distance)
-    }).catch ((error) => {
-      console.log(error);
-    });
-  }, [])
+  const initMarker = () => {
+    map.removeLayer(currentMarker);  
+    map.removeLayer(pickupMarker);  
+    map.removeLayer(dropoffMarker);  
+  }
 
   useEffect(() => {
     const markerTypes = {
@@ -106,92 +71,53 @@ const LeafletRoutingMachine = ({ type }) => {
 
   useEffect(() => {
     if (current[0] !== null && pickup[0] !== null && dropoff[0] !== null) {
+      //initMarker();
       if (routingControl) {
         map.removeControl(routingControl);
       }
 
-      const newRoutingControl = L.Routing.control({
-        waypoints: [L.latLng(current[0], current[1]), L.latLng(pickup[0], pickup[1]), L.latLng(dropoff[0], dropoff[1])],
-        lineOptions: {
-          styles: [{ color: "blue", weight: 5, opacity: 0.7 }],
-        },
-        router: L.Routing.osrmv1({
-          serviceUrl: "https://router.project-osrm.org/route/v1",
-          profile: "car",
-          alternatives: false,
-        }),
-        routeWhileDragging: false,
-        geocoder: L.Control.Geocoder.nominatim(),
-        addWaypoints: false,
-        draggableWaypoints: false,
-        fitSelectedRoutes: true,
-        showAlternatives: false,
-      }).addTo(map);
-
-      newRoutingControl.on("routeselected", async function (e) {
-        const route = e.route;
-        const distance = route.summary.totalDistance;
-        const coordinates = route.coordinates.map(coord => [coord.lat, coord.lng]);
-        const duration = route.summary.totalTime;
-        
-        setRouteData({ distance, duration, coordinates });
-        const distanceToFind = remainingFuelDistance * 1000;
-        
-        let accumulatedDistance = 0;
-        let markerPosition = null;
-
-        for (let i = 1; i < coordinates.length; i++) {
-          const [lat1, lng1] = coordinates[i - 1];
-          const [lat2, lng2] = coordinates[i];
-          const segmentDistance = map.distance([lat1, lng1], [lat2, lng2]);
-
-          accumulatedDistance += segmentDistance;
-
-          if (accumulatedDistance >= distanceToFind && !isGasStationFind) {
-            const ratio = (accumulatedDistance - distanceToFind) / segmentDistance;
-            const lat = lat2 - ratio * (lat2 - lat1);
-            const lng = lng2 - ratio * (lng2 - lng1);
-            markerPosition = [lat, lng];
-            break;
-          }
+      api.get("api/trip/addpoint", {
+        params: {
+          current_lat: current[0], 
+          current_lng: current[1],
+          pickup_lat: pickup[0],
+          pickup_lng: pickup[1],
+          dropoff_lat: dropoff[0],
+          dropoff_lng: dropoff[1]
         }
+      }).then ((response) => {
+        console.log(response.data);
+        
+        const waypoints = response.data.waypoints.map(point => L.latLng(point.lat, point.lng));
+        
+        response.data.waypoints.map((point) => {
+          L.marker([point.lat, point.lng])
+          .addTo(map)
+          .bindTooltip(point.label, { permanent: true, direction: "top" });
+        });
+        const newRoutingControl = L.Routing.control({
+          waypoints: waypoints,
+          lineOptions: {
+            styles: [{ color: "blue", weight: 5, opacity: 0.7 }],
+          },
+          router: L.Routing.osrmv1({
+            serviceUrl: "https://router.project-osrm.org/route/v1",
+            profile: "car",
+            alternatives: false,
+          }),
+          routeWhileDragging: false,
+          geocoder: L.Control.Geocoder.nominatim(),
+          addWaypoints: false,
+          draggableWaypoints: false,
+          fitSelectedRoutes: true,
+          showAlternatives: false,
+        }).addTo(map);
 
-        if (markerPosition && !isGasStationFind) {  
-          const gasStations = await getGasStations(markerPosition[0], markerPosition[1], 10000);
-  
-          let closestStation = null;
-          let closestDistance = Infinity;
-
-          gasStations.forEach(station => {
-            try {
-              const stationPosition = [station.lat, station.lng];
-              const distanceToStation = map.distance(markerPosition, stationPosition);
-
-              if (distanceToStation < closestDistance) {
-                closestDistance = distanceToStation;
-                closestStation = station;
-              }
-            } catch (error) {
-              // ignored
-            }
-          });
-
-          if (closestStation) {
-            const { lat, lng, name } = closestStation;
-            if (!route.waypoints.some(waypoint => waypoint.lat === lat && waypoint.lng === lng) && !isGasStationFind) {
-              newRoutingControl.spliceWaypoints(1, 0, L.latLng(lat, lng));
-              L.marker([lat, lng])
-              .addTo(map)
-              .bindPopup(`refueling - ${name}`, { permanent: true, direction: "top" });
-
-              setGasStationFind(true)
-            }
-          }
-
-        }
+        setRoutingControl(newRoutingControl);
+        
+      }).catch((error) => {
+        console.log(error);
       });
-
-      setRoutingControl(newRoutingControl);
     }
   }, [current, pickup, dropoff]);
 
